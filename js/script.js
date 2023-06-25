4fe4e4c701fcf30d6a611a0a904cef39f84fa42a
Sanju Shah <sanjushahgupta@gmail.com>
2023-06-26 00:28:51 +0200

    delted other pages and make website vertical scrollable and add respective content in indx.html

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 var toggleBtn = document.querySelector(".togglebtn");
-var nav = document.querySelector(".navlinks");
-var links = document.querySelector(".navlinks li");
+var links = document.querySelector("nav .navlinks");
 var animateText = document.querySelector(".animateText");
 
 toggleBtn.addEventListener("click", function () {
     this.classList.toggle("clicked");
-    nav.classList.toggle("open");
-
+    if (links.style.display === "block") {
+        links.style.display = "none";
+    } else {
+        links.style.display = "block";
+    }
 })
 
 var typed = new Typed(".animateText", {
-    strings: ["Web Developer", "Android Developer", "Web and Android Developer"],
+    strings: ["Android Developer", "Full Stack Developer"],
     typeSpeed: 50,
     loop: true,
-    backDelay: 900,
-    backSpeed: 30
+    backSpeed: 30,
 });
-
