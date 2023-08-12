db21ef1ac176114490e9d625f550db9e4f560f36
Sanju Shah <sanjushahgupta@gmail.com>
2023-08-12 20:29:17 +0200

    feat: add meet event project

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var toggleBtn = document.querySelector(".togglebtn");
 var links = document.querySelector("nav .navlinks");
 var animateText = document.querySelector(".animateText");
+var modal = document.querySelectorAll(".my-modal");
+var detailBtns = document.querySelectorAll(".detailsBtn")
+
+
 
 toggleBtn.addEventListener("click", function () {
     this.classList.toggle("clicked");
@@ -11,9 +15,23 @@ toggleBtn.addEventListener("click", function () {
     }
 })
 
+detailBtns.forEach(function (detailBtn, index) {
+    detailBtn.addEventListener("click", function () {
+        modal[index].style.display ="block"
+    })
+    window.addEventListener("mousedown", function(event) {
+        if (!modal[index].contains(event.target)) {
+            modal[index].style.display = "none";
+        }
+    });
+})
+ 
+
 var typed = new Typed(".animateText", {
     strings: ["Android Developer", "Full Stack Developer"],
     typeSpeed: 50,
     loop: true,
     backSpeed: 30,
 });
+
+
