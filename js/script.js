1e835a8c28e56ed496f7aefd220c9a6c4c383617
Sanju Shah <sanjushahgupta@gmail.com>
2023-10-12 21:41:54 +0200

    chore: update contact form

--- js/script.js
+++ js/script.js
@@ -2,30 +2,27 @@ var toggleBtn = document.querySelector(".togglebtn");
 var links = document.querySelector("nav .navlinks");
 var animateText = document.querySelector(".animateText");
 var modal = document.querySelectorAll(".my-modal");
-var detailBtns = document.querySelectorAll(".detailsBtn")
-
-
+var detailBtns = document.querySelectorAll(".detailsBtn");
 
 toggleBtn.addEventListener("click", function () {
-    this.classList.toggle("clicked");
-    if (links.style.display === "block") {
-        links.style.display = "none";
-    } else {
-        links.style.display = "block";
-    }
-})
+  this.classList.toggle("clicked");
+  if (links.style.display === "block") {
+    links.style.display = "none";
+  } else {
+    links.style.display = "block";
+  }
+});
 
 detailBtns.forEach(function (detailBtn, index) {
-    detailBtn.addEventListener("click", function () {
-        modal[index].style.display ="block"
-    })
-    window.addEventListener("mousedown", function(event) {
-        if (!modal[index].contains(event.target)) {
-            modal[index].style.display = "none";
-        }
-    });
-})
- 
+  detailBtn.addEventListener("click", function () {
+    modal[index].style.display = "block";
+  });
+  window.addEventListener("mousedown", function (event) {
+    if (!modal[index].contains(event.target)) {
+      modal[index].style.display = "none";
+    }
+  });
+});
 
 /*var typed = new Typed(".animateText", {
     strings: ["Android Developer", "Full Stack Developer"],
@@ -33,5 +30,3 @@ detailBtns.forEach(function (detailBtn, index) {
     loop: true,
     backSpeed: 30,
 });*/
-
-
